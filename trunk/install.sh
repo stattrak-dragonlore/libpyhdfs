a351f1ee956516ff15180585ad87cf935776aa14
kofreestyler <kofreestyler@6e669ef4-a436-11de-8dbb-b5444cbab91d>
2009-09-23 04:26:00 +0000

    install

---
+++ trunk/install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#install java
+sudo apt-get install sun-java6-jdk
+
+#fetch hadoop
+wget http://labs.xiaonei.com/apache-mirror/hadoop/core/hadoop-0.20.1/hadoop-0.20.1.tar.gz
+tar xzf hadoop-0.20.1.tar.gz
+ 
+#check out the source
+svn co http://libpyhdfs.googlecode.com/svn/trunk/ libpyhdfs
+
+#prepare library files
+cd hadoop-0.20.1
+cp c++/Linux-i386-32/liblibhdfs.so.0 hadoop-0.20.1-core.jar commons-logging-1.0.4.jar ../libpyhdfs/lib
+cd ../libpyhdfs
+ln -s libhdfs.so.0 lib/libhdfs.so
+  
+#install
+sudo python setup.py install --prefix=/usr/local
+
+#set CLASSPATH
+export LD_LIBRARY_PATH=/usr/local/lib/pyhdfs:/usr/lib/jvm/java-6-sun/jre/lib/i386/server
+export CLASSPATH=/usr/local/lib/pyhdfs/hadoop-0.20.1-core.jar:/usr/local/lib/pyhdfs/commons-logging-1.0.4.jar:/etc/pyhdfs
+
+#run test script
+cd test
+python pyhdfs_test.py
+   
